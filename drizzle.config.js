7a3aab21602d9a87f6970e31ecbded6ca1e1c6ac
Dev Trivedi <trivedidevdutt668@gmail.com>
2026-09-23 03:35:43 +0530

    Configure dotenv for drizzle-kit and add .env.example

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,8 +1,12 @@
-/** @type { import("drizzle-kit").Config } */
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+config({ path: ".env.local" });
+
 export default {
-    schema: "./utils/schema.js",
-    dialect: 'postgresql',
-    dbCredentials: {
-      url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
-    }
-  };
+  schema: "./utils/schema.js",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
+  },
+};
